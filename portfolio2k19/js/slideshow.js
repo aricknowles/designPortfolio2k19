d8aea7e24e4fc7a9c4b07a2c36cce5956ff5abe9
aricknowles <32400511+aricknowles@users.noreply.github.com>
2019-02-19 23:15:59 -0500

    boop

---
+++ portfolio2k19/js/slideshow.js
@@ -0,0 +1,18 @@
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("");
+  }
+  x[slideIndex-1].style.display = "block";
+}
